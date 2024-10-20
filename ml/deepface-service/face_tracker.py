b65908513f6dde0b712b89d63f93a4d9af403593
Tetraslam <bhowmickshresht@gmail.com>
2024-10-20 13:37:22 -0400

    ignore test

--- ml/deepface-service/face_tracker.py
+++ ml/deepface-service/face_tracker.py
@@ -1,6 +1,13 @@
 from deepface import DeepFace as face
 
+
+
+
 result = face.verify(
-  img1_path = "img1.jpg",
-  img2_path = "img2.jpg",
+  img1_path = "./test/rose.jpg",
+  img2_path = "./test/roseandbruno.jpg",
 )
+
+print(result)
+
+#face.stream(db_path = "./test/database")
